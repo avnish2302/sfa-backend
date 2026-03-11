41dcdb461a138960e2f0020d40f39995096f29b7
avnish <avnish2302@gmail.com>
2026-03-11 16:41:59 +0530

    implement refresh token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,44 +4,95 @@ import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   try {
-    const { name, email, password } = req.body
-    const [existingUser] = await pool.query("SELECT id FROM users WHERE email = ?",[email])
-    if (existingUser.length > 0) return res.status(400).json({ message: "Email already registered" })
-    const hashedPassword = await bcrypt.hash(password, 10)
-    const [result] = await pool.query("INSERT INTO users(name, email, password) VALUES(?, ?, ?)",[name, email, hashedPassword],)
-    res.status(201).json({ message: "User registered successfully" })
+    const { name, email, password } = req.body;
+    const [existingUser] = await pool.query(
+      "SELECT id FROM users WHERE email = ?",
+      [email],
+    );
+    if (existingUser.length > 0)
+      return res.status(400).json({ message: "Email already registered" });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const [result] = await pool.query(
+      "INSERT INTO users(name, email, password) VALUES(?, ?, ?)",
+      [name, email, hashedPassword],
+    );
+    res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
-    res.status(500).json({ error: error.message })              
+    res.status(500).json({ error: error.message });
   }
-}
+};
 
 export const login = async (req, res) => {
   try {
-    const { email, password } = req.body
-    const [rows] = await pool.query("SELECT * FROM users WHERE email = ?", [email]);
-    if (rows.length === 0) return res.status(400).json({ message: "User not found" })
-    const user = rows[0]
-    const isMatch = await bcrypt.compare(password, user.password)
-    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" })
-    const token = jwt.sign(
+    const { email, password } = req.body;
+
+    const [rows] = await pool.query("SELECT * FROM users WHERE email = ?", [
+      email,
+    ]);
+
+    if (rows.length === 0)
+      return res.status(400).json({ message: "User not found" });
+
+    const user = rows[0];
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    // ACCESS TOKEN
+    const accessToken = jwt.sign(
       { id: user.id, role: user.role },
-      process.env.JWT_SECRET,            
-      { expiresIn: "1d" },
-    )
-    res.json({ token })
+      process.env.JWT_SECRET,
+      { expiresIn: "15m" },
+    );
+
+    // REFRESH TOKEN
+    const refreshToken = jwt.sign({ id: user.id }, process.env.REFRESH_SECRET, {
+      expiresIn: "7d",
+    })
+    res.json({
+      accessToken,
+      refreshToken,
+    })
   } catch (error) {
-  console.error("LOGIN ERROR:", error);
-  res.status(500).json({ error: error.message || "Server error" });
-}
+    console.error("LOGIN ERROR:", error);
+    res.status(500).json({ error: error.message || "Server error" });
+  }
 }
 
+export const refreshAccessToken = async (req, res) => {
+  const { refreshToken } = req.body;
+
+  if (!refreshToken)
+    return res.status(401).json({ message: "Refresh token required" });
+
+  try {
+    const decoded = jwt.verify(refreshToken, process.env.REFRESH_SECRET);
+
+    const newAccessToken = jwt.sign(
+      { id: decoded.id },
+      process.env.JWT_SECRET,
+      { expiresIn: "15m" },
+    );
+
+    res.json({ accessToken: newAccessToken });
+  } catch (error) {
+    res.status(403).json({ message: "Invalid refresh token" });
+  }
+};
 
 export const getCurrentUser = async (req, res) => {
   try {
     const userId = req.user.id;
-    const [rows] = await pool.query("SELECT id, name, email, role FROM users WHERE id = ?",[userId])
-    if (rows.length === 0) return res.status(404).json({ message: "User not found" });
-    res.json(rows[0])
-  } 
-  catch (error) { res.status(500).json({ message: "Server error" })}
-}
+    const [rows] = await pool.query(
+      "SELECT id, name, email, role FROM users WHERE id = ?",
+      [userId],
+    );
+    if (rows.length === 0)
+      return res.status(404).json({ message: "User not found" });
+    res.json(rows[0]);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login } from "../controllers/authController.js";
+import { register, login, refreshAccessToken } from "../controllers/authController.js";
 import { getCurrentUser } from "../controllers/authController.js";
 import protect from "../middleware/authMiddleware.js";
 
@@ -7,6 +7,7 @@ const router = express.Router();               // creates mini router
 
 router.post("/register", register);            // POST request for registration. Calls register function
 router.post("/login", login);                  // POST request for login. Calls login function   
+router.post("/refresh", refreshAccessToken)
 
 router.get("/me", protect, getCurrentUser);
 
